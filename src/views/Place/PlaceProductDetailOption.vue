211e44afb8fdf063d0c69e161974e6a96968b3cb
JJB <hot805@intocns.com>
2026-01-09 17:25:38 +0900

    상품 관리(등록, 수정) > 옵션 연결 작업(작업중)

--- src/views/Place/PlaceProductDetailOption.vue
+++ src/views/Place/PlaceProductDetailOption.vue
@@ -7,19 +7,21 @@ import icDragHandel from '@/assets/icons/ic_drag_handel.svg'
 import icCheckOff from '@/assets/icons/ic_check_mark_off.svg'
 import icEmpty from '@/assets/icons/ic_empty.svg'
 
-import { onMounted } from 'vue';
+import { onMounted, computed } from 'vue';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const optionStore = useOptionStore();
+const router = useRouter();
+
+//PlaceProductDetail.vue에서 선언한component 옵션 사용
+const props = defineProps({
+    savedItemId: String
+})
 
 // 현재 어떤 카테고리가 열려있는지 index를 저장 (null이면 모두 닫힘)
 const expandedIndexes = ref([]); // 열려있는 인덱스들을 담는 배열
 
-// 카테고리 > 옵션 데이터 (임시)
-const tempOptionList = [
-    { id: '1', name: '신규 방문', desc: '처음 방문하시거나, 새로운 동물과 방문하시는 경우 선택해 주세요.', selected: true },
-    { id: '2', name: '재방문', desc: '동일한 동물과 재방문 하시는 경우 선택해 주세요.', selected: true }
-]
 // 아코디언 토글 함수
 const toggleAccordion = (index) => {
     const findIdx = expandedIndexes.value.indexOf(index);
@@ -36,15 +38,60 @@ const toggleAccordion = (index) => {
 // 특정 인덱스가 열려있는지 확인하는 함수 (템플릿 바인딩용)
 const isOpen = (index) => expandedIndexes.value.includes(index);
 
-onMounted(() => {
-    optionStore.getCategoryList()     
+//옵션 연결 > 저장 버튼
+const saveItemOption = (async() => {
+    let params = optionStore.optionList.flatMap(category =>
+                category.options
+                .filter(option => option.checked)
+                .map(option => ({
+                    categoryId : category.categoryId,
+                    optionId : option.optionId,
+                    useFlag : 1,
+                    itemIds : props.savedItemId
+                })
+            )
+    )
+
+    let response = await optionStore.addOptionMapping(params);
+
+    if(response.status_code <= 300){
+        alert('성공');
+    }else{
+        alert('실패');
+    }
+    
+    console.log(response);
 })
+
+onMounted(async() => {
+    // await optionStore.getCategoryList()
+    await optionStore.getOptionList()
+
+    optionStore.optionList = (optionStore.optionList ?? []).map(item => ({
+        ...item,
+        options : item.options.map(detail => ({
+            ...detail,
+            checked : false
+        }))
+    }))
+
+    expandedIndexes.value = (optionStore.optionList ?? []).map((_, index) => index)//아코디언 토글 디폴트로 열어주기 위함
+})
+
+//옵션이 하나라도 선택되면 카테고리도 선택
+const isCategoryChecked = (category) => computed(() => category.options.some(c => c.checked))
+
+//카테고리 체크박스 선택 시
+const changeCategoryChecked = (category) => {
+  const newVal = !isCategoryChecked(category).value
+  category.options.forEach(c => c.checked = newVal)
+}
 </script>
 
 <template>
     <p class="heading-s">예약받을 옵션을 선택 해 주세요.</p>
 
-    <template v-if="optionStore.categoryList.length > 0">
+    <template v-if="(optionStore.optionList ?? []).length > 0">
         <div class="product-detail-option-wrapper">
             <!-- left -->
             <div class="option-selection-container">
@@ -52,16 +99,18 @@ onMounted(() => {
                 <div class="category-section">
         
                     <div 
-                        v-for="(category, cIdx) in optionStore.categoryList" 
-                        :key="category.id"
+                        v-for="(option, cIdx) in optionStore.optionList" 
+                        :key="option.categoryId"
                         class="category-item"
                     >
                         <div class="category-header" @click="toggleAccordion(cIdx)">
                             <div class="d-flex align-center gap-8">
                                 <label class="checkbox" @click.stop>
-                                    <input type="checkbox" v-model="category.checked" />
+                                    <input type="checkbox" 
+                                        :checked="isCategoryChecked(option).value"
+                                        @change="() => changeCategoryChecked(option)" />
                                     <span class="box"></span>
-                                    <span class="label">{{ category.name }}</span>
+                                    <span class="label">{{ option.name }}</span>
                                 </label>
                             </div>
                             <img 
@@ -74,32 +123,35 @@ onMounted(() => {
         
                         <div v-show="isOpen(cIdx)" class="category-contents">
                             <ul class="option-list">
-                                <li v-for="option in tempOptionList" :key="option.id" class="option-item">
+                                <li v-for="optionDetail in option.options" :key="optionDetail.optionId" class="option-item">
                                     <div class="drag-handle">
                                         <img :src="icDragHandel" alt="정렬" />
                                     </div>
                                     
                                     <div class="option-info">
-                                        <p class="option-name title-m">{{ option.name }}</p>
-                                        <p class="option-desc body-xs">{{ option.desc }}</p>
+                                        <p class="option-name title-m">{{ optionDetail.name }}</p>
+                                        <p class="option-desc body-xs">{{ optionDetail.desc }}</p>
                                     </div>
         
                                     <div class="option-status">
-                                        <img :src="icCheckOff" v-if="option.selected" />
+                                        <label class="checkbox" @click.stop>
+                                            <input type="checkbox" v-model="optionDetail.checked" />
+                                            <span class="box"></span>
+                                        </label>
+                                        <!-- <img :src="icCheckOff" v-if="option.selected" /> -->
                                     </div>
                                 </li>
                             </ul>
-        
                         </div>
                     </div>
-        
+
                 </div>
         
             </div>
             
             <div class="button-wrapper">
-                <button class="btn btn--size-40 btn--black">목록으로</button>
-                <button class="btn btn--size-40 btn--blue">저장</button>
+                <button class="btn btn--size-40 btn--black" @click="router.push({ name: 'placeProduct'})">목록으로</button>
+                <button class="btn btn--size-40 btn--blue" @click="saveItemOption()">저장</button>
             </div>
         </div>
     </template>
@@ -110,12 +162,12 @@ onMounted(() => {
                 <img :src="icEmpty" alt="비어있음 아이콘">
                 <span class="title-s">등록된 옵션이 없습니다.</span>
                 <p class="body-m">예약 상품에 연결할 옵션을 등록 후 연결해주세요.</p>
-                <button class="btn btn--size-32 btn--black">옵션 등록</button>
+                <button class="btn btn--size-32 btn--black" @click="router.push({ name: 'placeOption'})">옵션 등록</button>
             </div>
         </div>
 
         <div class="empty-button-wrapper">
-            <button class="btn btn--size-40 btn--black">목록으로</button>
+            <button class="btn btn--size-40 btn--black" @click="router.push({ name: 'placeProduct'})">목록으로</button>
         </div>
     </template>
 </template>
