dad716e95d49de6adf900f925a57dfd1b3419d32
Hyerin-Min <rin1401@intocns.com>
2026-01-27 09:59:58 +0900

    전체예약 조회 > 테이블 행 클릭시, 예약정보 모달 노출

--- src/views/Reservation/ReservationListPage.vue
+++ src/views/Reservation/ReservationListPage.vue
@@ -232,8 +232,8 @@ onMounted(async () => {
                 </template>
                 <!-- 버튼 -->
                 <template #actions="{ row, rowIndex }">
-                        <button class="btn btn--size-24 btn--black-outline" @click="handelReserveDetail">상세</button>
-                        <button class="btn btn--size-24 btn--black-outline" @click="openSmsModal(row)"><img :src="icSms" alt="SMS"></button>
+                        <button class="btn btn--size-24 btn--black-outline" @click.stop="handelReserveDetail(row)">상세</button>
+                        <button class="btn btn--size-24 btn--black-outline" @click.stop="openSmsModal(row)"><img :src="icSms" alt="SMS"></button>
                 </template>
             </CommonTable>
         </template>
