01451ba58e68e2d1764a24bfa4686f5b3611c305
YoonHoGit <lolo18@intocns.com>
2026-02-09 08:26:32 +0900

    임시 개발 ID 키 적용

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,4 +1,8 @@
 export const COCODE = '2592'; // TODO: 개발 사용 임시 cocode
+export const HOS_IDX = '26'; // TODO: 개발 사용 임시 hosIdx
+
+// export const COCODE = '2919'; // TODO: 개발 사용 임시 cocode
+// export const HOS_IDX = '16'; // TODO: 개발 사용 임시 hosIdx
 
 export const PET_GENDER_MAP = {
     1: 'Male',
