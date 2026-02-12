51bd281d57a2e7a402025c793cdd1d6dc5b64d08
Hyerin-Min <rin1401@intocns.com>
2026-02-12 10:56:24 +0900

    네이버 연동 계정 관리 > 하드코딩 cocode 수정

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,9 +1,3 @@
-export const COCODE = '2592'; // TODO: 개발 사용 임시 cocode
-export const HOS_IDX = '26'; // TODO: 개발 사용 임시 hosIdx
-
-// export const COCODE = '2919'; // TODO: 개발 사용 임시 cocode
-// export const HOS_IDX = '16'; // TODO: 개발 사용 임시 hosIdx
-
 export const PET_GENDER_MAP = {
     1: 'Male',
     2: 'FeMale',
