4428d5a0f6ffffdd8c7e104381d4d73f1b483c0e
YoonHoGit <lolo18@intocns.com>
2026-02-24 07:32:17 +0900

    주석 삭제

--- src/utils/alimtalkSmsTemplate.js
+++ src/utils/alimtalkSmsTemplate.js
@@ -19,7 +19,6 @@ export const buildTemplateVariables = (reservationData = {}) => {
         '날짜': reservationData?.reservationDate,
         '예약시간': reservationData?.reservationTime,
         '상품명': reservationData?.productName,
-        // TODO: cocode로 병원 전화번호를 조회해 실제 값으로 교체
         '병원전화번호': reservationData?.hospitalPhone,
         '병원명': reservationData?.hospitalName,
         '고객명': reservationData?.protectorName,
